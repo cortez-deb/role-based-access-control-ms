c30cfe74d40993bd37d3e5bde8a3374c8f102c27
cortez-deb <lutalidavid44@gmail.com>
2024-12-01 14:28:12 +0300

    update

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ const __dirname = process.cwd();
 
 const sequelize = new Sequelize({
     database: 'kuza',
-  username: 'postgres',
-  password: '1234',
-  host: '127.0.0.1',
+  username: 'kuza',
+  password: 'UQS8PyW5nwqVOEADxmGQCCyRGEzHpPLs',
+  host: 'dpg-ct63iko8fa8c73c4ehrg-a.virginia-postgres.render.com',
   port: '5432',
 
    // database: process.env.DB_NAME,
